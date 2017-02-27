e9858538b0edcfa4aa49a5d41537548e4e4e50cb
HD_HOME <137590629@QQ.COM>
2017-02-27 22:05:12 +0800

    类的方法

--- Day2/类的方法.py
+++ Day2/类的方法.py
@@ -18,9 +18,24 @@ class Animal(object):
     def habit(self):
         print("这个方法是对象的属性")
 
+    #下面是给方法设置赋值和取值属性
+    @property
+    def play_total(self):
+        return self.num
+
+    @play_total.setter
+    def play_total(self,num):
+        self.num = num
+    @play_total.deleter  #加一个删除属性
+    def play_total(self):
+        del self.num
 a = Animal('HouDong')
 
 a.walkTwo(a) #传入自己的方法
 a.habit #方法加特性之后成为属性
 a.walk() #调用没有参数的sttaic methon
 a.talk() #不可以访问实例变量
+a.play_total = 12
+del a.play_total #删除某个变量
+n = a.play_total
+print("Seting Play_tital %d"%(n))
