2fe08d33c64c48b219c8ebedf272e6633ca5de33
HD <1105061266@qq.com>
2017-02-28 11:45:52 +0800

    字典和for循环

---
+++ For/For_Practice.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+2017-02-28 11:23:20
+HD
+FOR练习学习
+'''
+#循环输出大写字母
+for c in 'spam':
+    print(c.upper())
+#输出一个数的平方
+for x in range(6):
+    print(x**2)
