ffbbe92fd849a192d4cdbffd41c16929785bd7f9
HD <1105061266@qq.com>
2017-03-02 16:38:42 +0800

    修改连接到多线程TCP

--- Socket/ClientServer.py
+++ Socket/ClientServer.py
@@ -4,7 +4,7 @@
    Name:HD
 '''
 import socket
-ip_point=('127.0.0.1',8811)
+ip_point=('127.0.0.1',8813) #连接到socket多线程
 #创建实例对象
 sk = socket.socket()
 #设置访问的服务器ip地址
@@ -13,7 +13,9 @@ sk.connect(ip_point)
 sk.sendall(bytes('呼叫!不叫!','utf8'))
 #接受服务器返回的数据
 client_data = sk.recv(1024)
+
 #打印出服务器返回的数据
 print(str(client_data,'utf8'))
+print("发送成功！！！")
 #关闭客户端
 sk.close()
