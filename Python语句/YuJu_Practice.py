33dda53533f0ccd9376e7ba8614ed3fb4f2252a8
HD <1105061266@qq.com>
2017-03-16 16:43:33 +0800

    Python 基本语句

--- Python语句/YuJu_Practice.py
+++ Python语句/YuJu_Practice.py
@@ -110,7 +110,111 @@ r'''
 
     a,b,c = sting[0],sting[1],sting{2:]
 
+'''
+
+r'''
+    通常一个函数的返回值是None,在函数不返回人何有意义内容的函数默认返回的就是None
+'''
+r'''
+    调用格式：
+        从语法上讲,调用3.0的print函数有如下的形式
+        print([object,.....][,sep=' '][,end='\n'][file=sys.stdout])
+        在上面的函数中,方括号中的选项是可选的,并且可能会在一个给定的调用中省略，并且=后面的值都给出了参数的默认值。这个内置的函数把字符串sep所分开的一个或多个对象的文本表示，后面跟着的字符串end,都打印到流file中
+        sep,end 和file 部分如果给出的话，必须作为参数的关键字参数给定，也就是说必须使用一种特殊的"name=value"的语法来传递参数。
+
+        sep:
+            是在每一个对象的文本之间插入的一个字符串，如果没有传递的话,他默认地市一个单个的空格，传递一个空格它会抑制分隔符
+            print(a,b,c,d,sep='end') #打印a,b,c,d 的时候每个输出的变量之间都用end 进行分割
+
+        end:
+            是添加在打印文本末尾的一个字符串,如果没有传递它默认是一个\n换行符。
+        file:
+            file指定了文本将要发送到的文件,标准流或者其它类似文件的地方，如果没有传递它默认的是sys.stdout，带有类似文件的write(string)方法的任何对象都可以进行传递，但真正的文件应该为了输出而打开
+            print(x,y,z,sep='...',file==open('data.text','w')) #把打印输出到文本
+'''
+
+#打印 99 乘法表
+def main():
+    for  i in range(1,10):
+        for j in range(1,(i+1)):
+            print("%d * %d = %d\t"%(j,i,(i*j)),end='') # end  默认的是换行，现在设置成'' 不进行换行
+        print('')
+
+    return None
+
+if __name__ == '__main__':
+    main()
+
+r'''
+    python2.6和python 3.0 print语句的区别：
+        python2.6           python3.0           说明
+
+        print x,y           print(x,y)          默认
+
+        print x,y,          print(x,y,end='')   不在文本末尾添加'\n'
+
+        print >> afile,x,y  print(x,y,file=afile)   把文本发送搭配myfile.write 而不是sys.stdout.write
+'''
 
 
+r'''
+    其实print 语句只是python 的人性化的特性，提供sys.stdout 对象简单的接口，在加上默认的格式设置，实际上，如果想写的复杂一些，也可以直接使用sys模块输出
+    import sys
 
+    sys.stdout.write('Hello Word!\n')
+'''
+r'''
+if/else 三元表达式
+  if x:
+    a = y
+  else:
+    a = z
+
+可以携程
+a = y if x else z
+这个表达式和上面四行if语句的结果相同,但是更容易编写代码。当x为真的时候python会执行 表达式y，而当x为假的时候才会执行表达式z
+
+
+'''
+
+r'''
+ while和for 语句：
+    while是一个通用循环的一种方法，而for是用它来遍历序列对象内的元素，并对每个元素运行一个代码块
+    while语句最完整的书写格式是：
+                首行以及测试表达式，有一列或多列缩进语句的主题以及一个可选的else部分(控制权里快循环而又没碰到break语句时会执行)，python会一直计算winle开头的测试，然后执行循环主题内的语句，知道测试返回假为止
+
+        while <test>: 表达式
+            <statements1> 循环体语句
+        else: 可选else 语句，如果在循环体语句里没有碰到break 语句则执行这个语句
+            <<statements2> 执行代码语句
+
+    break,coninue pass 和循环else
+
+        break:
+            跳出最近所在的循环(跳过整个循环语句)
+        continue:
+            跳到最近所在循环的开头(到循环的表达式行)
+        pass:
+            什么事也不做，只是空占位符
+        循环else块：
+            只有当循环正常离开的时候才会执行（也就是说没有碰到break语句）
+
+    一般循环格式：
+        加入break和continue语句后，while 循环的一般格式如下：
+            while<test1>:
+                <statements1>
+                if <test2>:break
+                if <test3>:continue
+            else:
+                <statementd2>
+        break和continue 可以出现在while（或for）循环主体的任何地方，但通常会进一步嵌套在if语句中，根据某些条件来采取对应的操作
+
+    pass语句是无运算的占位语句，当语法需要语句并且还没有任何实用的语句可写时，就可以使用它。它通常用于为复合语句编写一个空的主体，例如想写个无限循环，每次迭代的时候什么也不做， 就写个pass
+        while True:pass
+
+        python3.0中可以使用...（三个连续的点号）来省略代码。由于省略号自身什么也不做，这可以当作pass 语句的一种替代方案
+    continue 语句会立刻跳到循环的顶端。
+
+    循环else:
+        和循环else子语句结合时，只有使用break语句跳出当前循环的时候才会执行循环else语句
 '''
