eacab8d3e4379ba60e2e41964e4e3d51ce7be1b6
HD <1105061266@qq.com>
2017-04-07 17:21:11 +0800

    RebibitMQ

--- RebbitMQ/RebbitMQ.py
+++ RebbitMQ/RebbitMQ.py
@@ -121,6 +121,21 @@ channel.exchange_declare(exchange='logs',
                          )
 
 message = 'Hello!Word'
+r'''
+    Type参数:
+    fanout
+    所有bind到此exchange的queue都可以接收消息
+    
+    direct
+    通过routingKey和exchange决定的那个唯一的queue可以接收消息
+    
+    topic
+    所有符合routingKey(此时可以是一个表达式)的routingKey所bind的queue可以接收消息
+    表达式符号说明：#代表一个或多个字符，*代表任何字符
+    例：#.a会匹配a.a，aa.a，aaa.a等
+    *.a会匹配a.a，b.a，c.a等
+    注：使用RoutingKey为#，Exchange Type为topic的时候相当于使用fanout
+'''
 
 channel.basic_publish(exchange='logs',
                       routing_key='',
