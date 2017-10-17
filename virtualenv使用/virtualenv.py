ccd272de99e6876f84cb3d92192accb1dc68aa21
HD <1105061266@qq.com>
2017-10-17 16:55:14 +0800

    添加collections模块的介绍！

---
+++ virtualenv使用/virtualenv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+Name:HD
+'''
+r'''
+    安装virtualenv  可以使用 sudo apt-get install python-virtualenv 进行安装
+    创建环境
+        virtualenv Test #test是沙盒环境的python
+    进入沙盒环境
+        source bin/activate   进入沙盒环境
+'''
