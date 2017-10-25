1a60a02847bfa46e7964735b784d6e5c2ab2dde4
HD <1105061266@qq.com>
2017-10-25 14:40:50 +0800

    SOCKET添加介绍

--- Socket/socket多线程.py
+++ Socket/socket多线程.py
@@ -21,6 +21,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
     #在下面处理客户端访问的数据，这个必须这样
     def handle(self):
         #拿到客户端发送过来的数据
+        #self.request 类里有所有socket里的方法，可以直接调用
         data = self.request.recv(1024)
         print("Client Says:",str(data,'utf8')) #处理编码
         self.request.send(bytes("收到！收到！！！",'utf8'))
@@ -34,6 +35,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
 if __name__ == "__main__":
     HOST,PORT = 'localhost',8813
-
+    #绑定ip地址和端口,和接收到客户端访问之后需要调用那个方法
     server = socketserver.ThreadingTCPServer((HOST,PORT),MyTCPHandler)
+    #开始监听调用端口
     server.serve_forever()
